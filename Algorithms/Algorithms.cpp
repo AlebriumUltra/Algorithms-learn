19d89620b2b0a6a31396dedb02aca9e23ee6b792
Limitless <Alebrium@gmail.com>
2020-09-10 22:02:18 +0300

    adding comments task 1,2,3,4

--- Algorithms/Algorithms.cpp
+++ Algorithms/Algorithms.cpp
@@ -1,57 +1,72 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
 #include "Algorithms.h"
 
-#define SIZE 3
+#define ROWS 10 // Константы для матрицы
+#define COLS 5
 
 
-typedef struct students {
-	char LastName[20];
-	char Name[20];
-	int NumberBook;
-	int Number;
-};
 
-void SearchStruct(students students[]) {
-	char SearchName[20];
-	printf("Enter name to search: ");
-	scanf("%s", SearchName);
-	bool flag = false;
-	for (int i = 0; i < SIZE; i++) {
-		if (strcmp(SearchName, students[i].Name) == 0) { // Поиск в структурах по имени
-			flag = true;
-			printf("Search success. Number struct is: %d\n", students[i].Number);
-			printf(" Name: %s\n Last name: %s\n Number book: %d", students[i].LastName, students[i].Name, students[i].NumberBook);
+void SearchMaxMin(int* Numbers, int Size) {
+	int Max, Min;
+	Max = Numbers[0]; // Присваиваем первый элемент массива
+	Min = Numbers[0];
+	for (int i = 0; i < Size - 1; i++) {
+		if (Max < Numbers[i + 1]) { // Сравниваем элементы и присваиваем наибольшее из 2-ух
+			Max = Numbers[i + 1];
+		}
+		if (Min > Numbers[i + 1]) { // Сравниваем элементы и присваиваем наименьшее из 2-ух
+			Min = Numbers[i + 1];
 		}
 	}
-	if (!flag) {
-		printf("Search failed.");
+	printf("\n  Min: %d\n  Max: %d", Min, Max);
+	printf("\n  Difference: %d", abs(Max - Min));
+}
+
+void RandomArray(int* Numbers, int Size) {
+	printf(" Array: ");
+	for (int i = 0; i < Size; i++) {
+		Numbers[i] = rand() % 201 - 100; // Заполнение массива случайными цифрами в диапазоне от -100 до 100
+		printf("%d ", Numbers[i]);
 	}
 }
 
-
-
-
-
-
-
-
-
-int main() {
-	students students[SIZE];
-	for (int i = 0; i < SIZE; i++) { // Инициализируем структуры
-		printf("Enter name: ");
-		scanf("%s", students[i].Name);
-
-		printf("Enter last name: ");
-		scanf("%s", students[i].LastName);
-
-		printf("Enter number book: ");
-		scanf("%d", &students[i].NumberBook);
-
-		students[i].Number = i + 1;
+void RandomMatrix(int Matrix[ROWS][COLS]) {
+	printf(" Matrix: \n\n");
+	for (int i = 0; i < ROWS; i++) {
+		for (int j = 0; j < COLS; j++) {
+			Matrix[i][j] = rand() % 201 - 100; // Заполнение массива случайными цифрами в диапазоне от -100 до 100
+			printf(" %d\t", Matrix[i][j]);
+		}
 		printf("\n");
 	}
+}
+
+int main() {
+	// Задание 1,2,3
+	srand(time(NULL));
+	int* Numbers;
+	int Size;
+	printf(" Enter size array: "); // Пользовательский ввод размера массива
+	scanf("%d", &Size);
+	Numbers = (int*)malloc(sizeof(int) * Size); // Выделение памяти
+	RandomArray(Numbers, Size); // Функция по инициализации массива случайными числами
+	printf("\n");
+	SearchMaxMin(Numbers, Size); // Поиск максимального и минимального элементов и вывод разницы
+	free(Numbers);
+	printf("\n");
+
+	// Задание 4
+	int Matrix[ROWS][COLS];
+	int SumCols = 0;
 	printf("\n\n");
-	SearchStruct(students);
+	RandomMatrix(Matrix); // Инициализвация массива случайными числами
+	printf("\n\n");
+	for (int j = 0; j < COLS; j++) { // Т.к считается сумма столбца, меняем циклы местами
+		for (int i = 0; i < ROWS; i++) {
+			SumCols += Matrix[i][j];
+		}
+		printf("Col (%d): %d\n", j + 1, SumCols);
+		SumCols = 0; // Обнуляем для считывания следующих столбцов
+	}
 	_getch();
 }
