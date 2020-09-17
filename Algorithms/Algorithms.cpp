ddc415fa0780c0f8e9f30e9bc1560885f2f9b9dd
Limitless <Alebrium@gmail.com>
2020-09-17 19:30:24 +0300

    Sorting functions

--- Algorithms/Algorithms.cpp
+++ Algorithms/Algorithms.cpp
@@ -2,79 +2,86 @@
 #include "Algorithms.h"
 
 
-void SearchMaxMin(int* Numbers, int Size) {
-	int Max, Min;
-	Max = Numbers[0]; // Присваиваем первый элемент массива
-	Min = Numbers[0];
-	for (int i = 0; i < Size - 1; i++) {
-		if (Max < Numbers[i + 1]) { // Сравниваем элементы и присваиваем наибольшее из 2-ух
-			Max = Numbers[i + 1];
+void shellsort(int* items, int count)
+{
+	int i, j, gap, k;
+	int x, a[5];
+	a[0] = 9; a[1] = 5; a[2] = 3; a[3] = 2; a[4] = 1;
+	for (k = 0; k < 5; k++) {
+		gap = a[k];
+		for (i = gap; i < count; ++i) {
+			x = items[i];
+			for (j = i - gap; (x < items[j]) & (j >= 0); j = j - gap)
+				items[j + gap] = items[j];
+			items[j + gap] = x;
 		}
-		if (Min > Numbers[i + 1]) { // Сравниваем элементы и присваиваем наименьшее из 2-ух
-			Min = Numbers[i + 1];
-		}
-	}
-	printf("\n  Min: %d\n  Max: %d", Min, Max);
-	printf("\n  Difference: %d", abs(Max - Min));
-}
-
-void RandomArray(int* Numbers, int Size) {
-	printf(" Array: ");
-	for (int i = 0; i < Size; i++) {
-		Numbers[i] = rand() % 201 - 100; // Заполнение массива случайными цифрами в диапазоне от -100 до 100
-		printf("%d ", Numbers[i]);
 	}
 }
 
-void RandomMatrix(int** Matrix, int Rows, int Cols) {
-	printf(" Matrix: \n\n");
-	for (int i = 0; i < Rows; i++) {
-		for (int j = 0; j < Cols; j++) {
-			Matrix[i][j] = rand() % 201 - 100; // Заполнение массива случайными цифрами в диапазоне от -100 до 100
-			printf(" %d\t", Matrix[i][j]);
+void qs(int* items, int left, int right) {
+	int i, j;
+	int x, y;
+	i = left; j = right;
+	x = items[(left + right) / 2];
+	do {
+		while ((items[i] < x) && (i < right))
+			i++;
+		while ((x < items[j]) && (j > left))
+			j--;
+		if (i <= j) {
+			y = items[i];
+			items[i] = items[j];
+			items[j] = y;
+			i++; 
+			j--;
 		}
-		printf("\n");
+	} while (i <= j);
+	if (left < j) 
+		qs(items, left, j);
+	if (i < right) 
+		qs(items, i, right);
+}
+
+void Randomized(int* items, int count) {
+	for (int i = 0; i < count; i++) {
+		items[i] = rand() % 30001 - 15000;
 	}
+}
+
+void Reversed(int* items, int count) {
+	for (int i = 0; i < count; i++) {
+		items[i] = count - i;
+	}
+}
+
+void Sorted(int* items, int count) {
+	for (int i = 0; i < count; i++) {
+		items[i] = i + 1;
+	}
+}
+
+void Saw(int* items, int count) {
+	Sorted(items, count / 2);
+	for (int i = count / 2; i < count; i++) {
+		items[i] = count - i;
+	}
+}
+
+void PrintArray(int* items, int count) {
+	for (int i = 0; i < count; i++) {
+		printf("%d ", items[i]);
+	}
+}
+
+void OutputFile(int* items, int count, float time) {
+	FILE* Output;
+	Output = fopen("results.txt", "");
 }
 
 int main() {
-	// Задание 1,2,3
 	srand(time(NULL));
-	int* Numbers;
-	int Size;
-	printf(" Enter size array: "); // Пользовательский ввод размера массива
-	scanf("%d", &Size);
-	Numbers = (int*)malloc(sizeof(int) * Size); // Выделение памяти
-	RandomArray(Numbers, Size); // Функция по инициализации массива случайными числами
-	printf("\n");
-	SearchMaxMin(Numbers, Size); // Поиск максимального и минимального элементов и вывод разницы
-	free(Numbers);
-	printf("\n");
-
-	// Задание 4
-	int Rows, Cols;
-	printf("Enter amount rows: ");
-	scanf("%d", &Rows);
-	printf("Enter amount cols: ");
-	scanf("%d", &Cols);
-	int** Matrix = (int**)malloc(sizeof(int) * Rows);
-	for (int i = 0; i < Rows; i++) {
-		Matrix[i] = (int*)malloc(sizeof(int) * Cols);
-	}
-	int SumCols = 0;
-	printf("\n\n");
-	RandomMatrix(Matrix, Rows, Cols); // Инициализвация массива случайными числами
-	printf("\n\n");
-	for (int j = 0; j < Cols; j++) { // Т.к считается сумма столбца, меняем циклы местами
-		for (int i = 0; i < Rows; i++) {
-			SumCols += Matrix[i][j];
-		}
-		printf("Col (%d): %d\n", j + 1, SumCols);
-		SumCols = 0; // Обнуляем для считывания следующих столбцов
-	}
-	for (int i = 0; i < Rows; i++) {
-		free(Matrix[i]);
-	}
-	free(Matrix);
+	int* Array = (int*)malloc(sizeof(int) * 10000);
+    Randomized(Array, 10000);
+    PrintArray(Array, 10000);
 	_getch();
 }
