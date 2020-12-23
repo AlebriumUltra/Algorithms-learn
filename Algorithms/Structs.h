4a96a77b467f0663135b687b10ab9d908668907d
Limitless <Alebrium@gmail.com>
2020-12-24 00:56:29 +0300

    clear

--- Algorithms/Structs.h
+++ Algorithms/Structs.h
@@ -1,18 +1,1 @@
 #pragma once
-
-typedef struct Graph {
-	int** Matrix;
-	int* Vertexes;
-	int MatrixOrder;
-};
-
-typedef struct Node {
-	int vertex;
-	Node* next;
-};
-
-typedef struct GraphList {
-	int numVertexes;
-	int* seqVertexes;
-	Node** Heads;
-};
